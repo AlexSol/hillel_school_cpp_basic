7ae615a01fd4c296bc49ad10116335438deec4b1
oplischenko <oplischenko@eva.ua>
2024-11-29 18:39:41 +0200

    update unit-6 add - virtual-override add - virtual-without-override

---
+++ unit-6/virtual-override.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+ 
+class A
+{
+public:
+	virtual const char* getName1(int x) { return "A"; }
+	virtual const char* getName2(int x) { return "A"; }
+	virtual const char* getName3(int x) { return "A"; }
+};
+ 
+class B : public A
+{
+public:
+	virtual const char* getName1(short int x) override { return "B"; }  // помилка компіляції, метод не є перевизначенням
+	virtual const char* getName2(int x) const override { return "B"; }  // помилка компіляції, метод не є перевизначенням
+	virtual const char* getName3(int x) override { return "B"; }  // все добре, метод є перевизначенням A::getName3(int)
+ 
+};
+ 
+int main()
+{
+	return 0;
+}
